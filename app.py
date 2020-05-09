06109f90833032f103f7934860b7f384a24240f3
Nachiketa Hebbar <54833985+nachi-hebbar@users.noreply.github.com>
2020-05-09 11:17:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField,RadioField,TextAreaField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import InputRequired
-import pickle
 
-from model import casual_test,build_wordlist,wordlist
 app=Flask(__name__)
 app.config['SECRET_KEY']='some_random_secret'
 
 class SignUpForm(FlaskForm):
-    news=StringField('Enter your News to be tested',validators=[InputRequired()])
+    name=StringField('Enter your Name',validators=[InputRequired()])
     email=EmailField('Enter your email',validators=[InputRequired()])
     publicity=RadioField('How did you hear about us ?',choices=[('social','Social Media'),('poster','Posters and flyers')])
     feedback=TextAreaField('Anything else you want to tell us ?')
@@ -21,7 +19,7 @@ def hello_world():
 
     form = SignUpForm()
     if form.validate_on_submit():
-        session['news'] = form.news.data
+        session['name'] = form.name.data
         session['email'] = form.email.data
         session['publicity'] = form.publicity.data
         session['feedback'] = form.feedback.data
@@ -31,8 +29,8 @@ def hello_world():
 
 @app.route('/thankyou')
 def thankyou():
-    pred = casual_test(session['news'], loaded_model, wordlist)
-    return render_template('thankyou.html',pred=pred)
+ 
+    return render_template('thankyou.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
